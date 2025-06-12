e1f4461d9cdfe0fe4fcf15fc4351c26dac31dc69
anand <anandmehta300@gmail.com>
2025-06-13 05:26:16 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from mlProject import logger 
+from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is an info message")
-
+STAGE_NAME = "Data Ingestion Stage"
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
