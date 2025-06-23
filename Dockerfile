b61f1aa74955d7084269a6525999637416374295
anand <anandmehta300@gmail.com>
2025-06-24 01:31:25 +0530

    added dockerfile

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.10-slim  
+
+RUN apt update -y && apt install awscli -y
+WORKDIR /app
+
+COPY . /app
+RUN pip install  -r requirements.txt
+
+
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8080"]
--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
+# setup.py
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-
-__version__ = "0.0.0"
-
-REPO_NAME = "AutoPrice-AI"
-AUTHOR_USER_NAME = "Anand singh"
-SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "anandmehta300@gmail.com"
-
-
 setuptools.setup(
-    name=SRC_REPO,
-    version=__version__,
-    author=AUTHOR_USER_NAME,
-    author_email=AUTHOR_EMAIL,
-    description="A small python package for ml app",
+    name="mlProject",
+    version="0.1.0",
+    author="Anand Singh",
+    author_email="anandmehta300@gmail.com",
+    description="A small python package for ML app",
     long_description=long_description,
-    long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    long_description_content_type="text/markdown",
+    url="https://github.com/anandmehta300/AutoPrice-AI",
     project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        "Bug Tracker": "https://github.com/anandmehta300/AutoPrice-AI/issues",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[],  # you can add dependencies here if needed
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
 )
